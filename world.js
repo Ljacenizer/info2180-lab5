fc97b6b843276c497db59b7ccc7d541552394981
Ljacenizer <jasonmcleggo@gmail.com>
2021-11-09 22:51:41 -0500

    Were out here onto math projects ;--;

--- world.js
+++ world.js
@@ -1,39 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    document.getElementById("lookup").addEventListener('click',(event) => {
-          event.preventDefault();
-          xmlhttpreq=new XMLHttpRequest();
-
-          xmlhttpreq.onreadystatechange = function(){
-          if(xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===200){
-            document.getElementById("result").innerHTML=xmlhttpreq.responseText;
-          }
-          if( xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===404){
-            alert("ERROR:");
-          }
-
-        }
-            xmlhttpreq.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value),true);
-            xmlhttpreq.send();
-
-      });
-
-      document.getElementById("lookupCities").addEventListener('click',(event) => {
+  document.getElementById("lookup").addEventListener('click',(event) => {
         event.preventDefault();
-        xmlhttpreq=new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
 
-        xmlhttpreq.onreadystatechange = function(){
-          if(xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===200){
-            document.getElementById("result").innerHTML=xmlhttpreq.responseText;
-          }
-          if( xmlhttpreq.readyState===XMLHttpRequest.DONE && xmlhttpreq.status===404){
-            alert("ERROR:");
-          }
-
+        xhr.onreadystatechange = function(){
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+          document.getElementById("result").innerHTML = xhr.responseText;
         }
-        xmlhttpreq.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value)+"context=cities",true);;
-          xmlhttpreq.send();
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 404){
+          alert("ERROR: " + xhr.status);
+        }
+      }
+          xhr.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value), true);
+          xhr.send();
+  });
 
-    });
+  document.getElementById("lookupCities").addEventListener('click',(event) => {
+      event.preventDefault();
+      xhr = new XMLHttpRequest();
 
-  }, false);
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState===XMLHttpRequest.DONE && xhr.status===200){
+          document.getElementById("result").innerHTML=xhr.responseText;
+        }
+        if( xhr.readyState===XMLHttpRequest.DONE && xhr.status===404){
+          alert("ERROR: " + xhr.status);
+        }
+      }
+      xhr.open("GET","http://localhost/info2180-lab5/world.php?country="+(document.getElementById("country").value)+"&context=cities", true);
+        xhr.send();
+  });
+}, false);
--- world.php
+++ world.php
@@ -5,17 +5,57 @@ $password = 'password123';
 $dbname = 'world';
 
 if($_SERVER['REQUEST_METHOD'] === 'GET' ){
-  if((isset($_GET['country']) or !empty($_GET['country'])) ){
+  if(empty($_GET['context']) and (isset($_GET['country']) or !empty($_GET['country'])) ){
     $country = filter_var($_GET['country'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);;
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Country Name</th>";
+    echo "<th>Continent</th>";
+    echo "<th>Independence Year</th>";
+    echo "<th>Head of State</th>";
+    echo "</tr>";
+    foreach($results as $row) {
+        $countryName = $row['name'];
+        $continent = $row['continent'];
+        $independence= $row['independence_year'];
+        $HOState = $row['head_of_state'];
+        echo "<tr>";
+        echo "<td>$countryName</td>";
+        echo "<td>$continent</td>";
+        echo "<td>$independence</td>";
+        echo "<td>$HOState</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    }
+}
+
+if ((isset($_GET['context']) or !empty($_GET['context']))) {
+    $country = filter_var($_GET['country'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT cities.name, cities.district,cities.population FROM countries LEFT JOIN cities ON countries.code = cities.country_code WHERE countries.name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>District</th>";
+    echo "<th>Population</th>";
+    echo "</tr>";
+    foreach($results as $row) {
+        $city = $row['name'];
+        $district = $row['district'];
+        $population = $row['population'];
+        echo "<tr>";
+        echo "<td>$city</td>";
+        echo "<td>$district</td>";
+        echo "<td>$population</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
 }
 ?>
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
